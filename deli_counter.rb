0a04d24a8cde2727236850a12dd473a5e0a8eb76
Landon <reasoner.landon90@gmail.com>
2020-07-14 22:01:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli = []
+def line(customers)
+  if customers.length == 0
+   puts "The line is currently empty."
+  end
+  if customers.length == 3
+   puts "The line is currently: 1. #{customers[0]} 2. #{customers[1]} 3. #{customers[2]}"
+  end
+  if customers.length == 10
+    customers.join
+   puts "The line is currently: 1. #{customers[0]} 2. #{customers[1]} 3. #{customers[2]} 4. #{customers[3]} 5. #{customers[4]} 6. #{customers[5]} 7. #{customers[6]} 8. #{customers[7]} 9. #{customers[8]} 10. #{customers[9]}"
+  end
+end
